443d58e5209fe291c3b0635c9741a6044b858a40
mRHeisen <miguel.avilla92@gmail.com>
2016-11-18 13:41:07 -0200

    Vamo Heroku

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var httpProxy = require('http-proxy');
 var app = express();
 
 app.use(express.static('./public'));
