a568b984b5a72e28619a36d2e174a6f7e0763f9b
mRHeisen <miguel.avilla92@gmail.com>
2016-11-17 23:33:24 -0200

    Atualizando

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http =  require('http');
 
 var app = require('./config/express');
-
+var port = process.env.PORT || 3000;
 require('./config/database')('localhost/storyteller_db');
-http.createServer(app).listen(3000, function(){
+http.createServer(app).listen(port, function(){
 	console.log("Serividor Rodando");
 });
