0f28e977be36a1facbfa2b730509400bc1a413e8
mRHeisen <miguel.avilla92@gmail.com>
2016-11-18 05:02:43 -0200

    Atualizando

--- app/routes/genero.js
+++ app/routes/genero.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
 
-	//var api = app.api.genero;
+	var api = app.api.genero;
 	
      app.route('/v1/generos')
         .get(api.lista) //Buscar recurso com get / espera get e uma funcção
